2726b38cc825c52cd479e4b6ba088fab8f4ff0df
Tushar Sharma <124043637+tusharsharm@users.noreply.github.com>
2025-07-30 05:13:15 +0000

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,17 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "@/pages/home";
+import Lobby from "@/pages/lobby";
+import Game from "@/pages/game";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
-      {/* Add pages below */}
-      {/* <Route path="/" component={Home}/> */}
-      {/* Fallback to 404 */}
+      <Route path="/" component={Home} />
+      <Route path="/lobby" component={Lobby} />
+      <Route path="/game" component={Game} />
       <Route component={NotFound} />
     </Switch>
   );
